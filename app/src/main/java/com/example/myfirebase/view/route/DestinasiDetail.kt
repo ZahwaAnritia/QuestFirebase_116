cd3ff3430ab15ccd221587ce6a24c7dc462d27ed
ZahwaAnritia <anritia15@gmail.com>
2026-01-02 16:28:46 +0700

    Menambahkan object destinasi detail

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiDetail.kt
@@ -1,4 +1,10 @@
 package com.example.myfirebase.view.route
 
-object DestinasiDetail {
+import com.example.myfirebase.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "item_detail"
+    override val titleRes = R.string.detail_siswa
+    const val siswaIdArg = "siswaId"
+    val routeWithArgs = "$route/{$siswaIdArg}"
 }
