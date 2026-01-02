76970d71a493f14083fece1a785910fa8d98570c
ZahwaAnritia <anritia15@gmail.com>
2026-01-02 16:31:26 +0700

    Menambahkan edit view model

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -1,2 +1,70 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.DetailSiswa
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.modeldata.toDataSiswa
+import com.example.myfirebase.modeldata.toUiStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import com.example.myfirebase.view.route.DestinasiEdit
+import kotlinx.coroutines.launch
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        checkNotNull(savedStateHandle[DestinasiEdit.siswaIdArg])
+
+
+    init {
+        loadSiswa()
+    }
+
+    private fun loadSiswa() {
+        viewModelScope.launch {
+            val siswa = repositorySiswa
+                .getDataSiswa()
+                .find { it.id == idSiswa }
+
+            siswa?.let {
+                uiStateSiswa = it.toUiStateSiswa(true)
+            }
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() &&
+                    alamat.isNotBlank() &&
+                    telpon.isNotBlank()
+        }
+    }
+
+    suspend fun editSatuSiswa() {
+        if (uiStateSiswa.isEntryValid) {
+            repositorySiswa.updateSiswa(
+                uiStateSiswa.detailSiswa.toDataSiswa()
+            )
+        }
+    }
+
+}
