abc2e4c7d1e0152267c4afb5a5a42607898f6c01
ZahwaAnritia <anritia15@gmail.com>
2026-01-08 19:43:15 +0700

    menambahkan fungsi update ui state

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -37,3 +37,10 @@ class EditViewModel(
         }
     }
 
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
