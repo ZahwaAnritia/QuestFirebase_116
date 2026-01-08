44a6a3a631e541f0a1cc50a0be476157b7ca14b5
ZahwaAnritia <anritia15@gmail.com>
2026-01-08 19:41:52 +0700

    menambahkan state dan id siswa pada EditViewModel

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -20,4 +20,12 @@ import kotlinx.coroutines.launch
 class EditViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositorySiswa: RepositorySiswa
-)
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
