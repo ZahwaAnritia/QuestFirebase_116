bd390bb8cba1f60058e0295daf4b5175c15fd4b7
ZahwaAnritia <anritia15@gmail.com>
2026-01-08 19:37:35 +0700

    update DestinasiEdit.kt

--- app/src/main/java/com/example/myfirebase/view/route/DestinasiEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/route/DestinasiEdit.kt
@@ -1,10 +1,12 @@
 package com.example.myfirebase.view.route
 
 import com.example.myfirebase.R
+import com.example.myfirebase.view.route.DestinasiNavigasi
 
 object DestinasiEdit : DestinasiNavigasi {
     override val route = "item_edit"
     override val titleRes = R.string.edit_siswa
-    const val siswaIdArg = "siswaId"
-    val routeWithArgs = "$route/{$siswaIdArg}"
+    const val itemIdArg = "itemId"
+
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
